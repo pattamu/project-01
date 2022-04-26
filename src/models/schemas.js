7b8b0aedf25313eab0f3db11955233afabc32559
pattamu <102967813+pattamu@users.noreply.github.com>
2022-04-26 18:51:23 +0530

    email validator added to authorSchema

--- src/models/schemas.js
+++ src/models/schemas.js
@@ -5,7 +5,19 @@ const authorSchema = mongoose.Schema({
     fname: { type: String, required: true}, 
     lname: {type: String, required: true}, 
     title: {type: String, enum: ["Mr", "Mrs", "Miss"], required: true}, 
-    email: {type: String, required: true, unique: true}, 
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        },
+        required: [true, "Email required"]
+    }, 
     password: {type: String, required: true} 
 }, { timestamps: true })
 
